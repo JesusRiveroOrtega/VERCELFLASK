3ea367c07dd8950eb37a7ef54cd2dc06e684196a
Jesus Rivero <jrivero.jesus@gmail.com>
2024-04-13 11:39:59 -0500

    manage files

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/hello')
+def hello_page():
+    return jsonify({"message" : "Hello"})
 
 
 if __name__ == '__main__':
